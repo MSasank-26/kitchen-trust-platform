0acc9ce8c523a267cfa8f3808e05b3624e38c0db
MSasank-26 <sasankmedepalli@gmail.com>
2026-05-22 18:51:12 +0530

    Fix production CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,17 +1,25 @@
 import { ValidationPipe } from '@nestjs/common';
+
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  
+  const app =
+    await NestFactory.create(AppModule);
+
   app.enableCors({
-  origin: 'http://localhost:3001',
-  credentials: true,
-});
+    origin: '*',
+    credentials: true,
+  });
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe(),
+  );
 
-  await app.listen(3000);
+  await app.listen(
+    process.env.PORT ?? 3000,
+  );
 }
+
 bootstrap();
