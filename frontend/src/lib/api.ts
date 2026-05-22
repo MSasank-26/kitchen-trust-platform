cc929d9cbf37404ef44642b2653889382962ca37
MSasank-26 <sasankmedepalli@gmail.com>
2026-05-22 18:44:04 +0530

    Configure production API URL

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -3,7 +3,8 @@ import axios from 'axios';
 import { useAuthStore } from '@/store/auth.store';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL,
 });
 
 api.interceptors.request.use((config) => {
